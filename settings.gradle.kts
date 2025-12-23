c869f6e164d2b762ba867c34476c5bd7146e9398
Mark Rode <rode.mark01@gmail.com>
2025-12-23 13:23:11 +0300

    fix version resolution and update shadow plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,10 @@
+import java.net.InetAddress
+import java.util.zip.CRC32
+
 pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.jvm") version ("2.0.21")
-        id("com.github.johnrengelman.shadow") version ("8.1.1")
+        id("com.gradleup.shadow") version ("8.3.6")
         id("com.avast.gradle.docker-compose") version ("0.16.9")
         id("io.github.gradle-nexus.publish-plugin") version ("1.1.0")
         id("org.octopusden.octopus.oc-template") version (extra["octopus-oc-template.version"] as String)
@@ -9,3 +12,10 @@ pluginManagement {
 }
 
 rootProject.name = "octopus-artifactory-automation"
+
+gradle.beforeProject {
+    project.version = gradle.startParameter.projectProperties["version"] ?: with(CRC32()) {
+        update(InetAddress.getLocalHost().hostName.toByteArray())
+        "$value-SNAPSHOT"
+    }
+}
